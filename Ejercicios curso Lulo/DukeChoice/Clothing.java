261d4229bfb7d19366a3e22e534eda2ddc984fca
scalulo <167205361+scalulo@users.noreply.github.com>
2025-05-04 22:30:07 -0300

    Update Clothing.java

--- Ejercicios curso Lulo/DukeChoice/Clothing.java
+++ Ejercicios curso Lulo/DukeChoice/Clothing.java
@@ -2,15 +2,15 @@ package EjerciciosCurso.DukeChoice;
 
 public class Clothing {
     private String description;
-    private double price;
-    private double minPrice = 10.0;
+    private double precio;
+    private double minPrecio = 10.0;
     private double tax = 1.2;
-    private String size = "M";
+    private String talle = "M";
 
-    public Clothing(String description, double price, String size) {
+    public Clothing(String description, double precio, String talle) {
         this.description = description;
-        this.price = price;
-        this.size = size;
+        this.precio = precio;
+        this.precio = talle;
     }
 
     public String getDescription() {
@@ -21,21 +21,21 @@ public class Clothing {
         this.description = description;
     }
 
-    public double getPrice() {
-        return price * tax;
+    public double getPrecio() {
+        return precio * tax;
     }
 
-    public void setPrice(double price) {
-        if (price > minPrice){
-            this.price = price;
+    public void setPrecio(double precio) {
+        if (precio > minPrecio){
+            this.precio = precio;
         }
     }
 
-    public String getSize() {
-        return size;
+    public String gettalle() {
+        return talle;
     }
 
-    public void setSize(String size) {
-        this.size = size;
+    public void setTalle(String talle) {
+        this.talle = talle;
     }
 }
