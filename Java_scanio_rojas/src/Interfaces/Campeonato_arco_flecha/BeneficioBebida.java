0da9673010c82e0a9faccc5f08b8866cda33b3f4
scalulo <alumno26.scanio.lautaro@ipm.edu.ar>
2025-09-08 12:40:05 -0300

    terminamos el1 y el 2 de interfaces

--- Java_scanio_rojas/src/Interfaces/Campeonato_arco_flecha/BeneficioBebida.java
+++ Java_scanio_rojas/src/Interfaces/Campeonato_arco_flecha/BeneficioBebida.java
@@ -3,19 +3,19 @@ package Interfaces.Campeonato_arco_flecha;
 import java.util.HashSet;
 
 public class BeneficioBebida extends Beneficio implements BeneficioAcumulable{
-    private HashSet<String> comidas;
+    private HashSet<String> bebidas;
 
-    public BeneficioBebida(String nombre_creador, String nombre_beneficio, HashSet<String> comidas) {
+    public BeneficioBebida(String nombre_creador, String nombre_beneficio, HashSet<String> bebidas) {
         super(nombre_creador, nombre_beneficio);
-        this.comidas = comidas;
+        this.bebidas = bebidas;
     }
 
-    public HashSet<String> getComidas() {
-        return comidas;
+    public HashSet<String> getbebidas() {
+        return bebidas;
     }
 
-    public void setComidas(HashSet<String> comidas) {
-        this.comidas = comidas;
+    public void setbebidas(HashSet<String> bebidas) {
+        this.bebidas = bebidas;
     }
 
     @Override
---
+++ Java_scanio_rojas/src/Interfaces/Tratamiento/Genero.java
@@ -0,0 +1,5 @@
+package Interfaces.Tratamiento;
+
+public enum Genero {
+    FEMENINO,MASCULINO,X;
+}
